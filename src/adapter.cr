560afde88a861b46c2eb5dba7c38e704b94d4db3
Oleksii Fedorov <waterlink000@gmail.com>
2015-04-16 00:45:44 +0300

    Fill out adapter interface

--- src/adapter.cr
+++ src/adapter.cr
@@ -1,5 +1,10 @@
 module ActiveRecord
   abstract class Adapter
-    abstract def create(table_name, fields)
+    abstract def create(fields)
+    abstract def read(id)
+    abstract def where(query_hash)
+    abstract def where(query, params)
+    abstract def update(id, fields)
+    abstract def delete(id)
   end
 end
