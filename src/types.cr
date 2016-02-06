8d7129dc524489ccd4d5c96af4dca1009056909c
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-06 21:55:13 +0100

    De-dup types definitions

--- src/types.cr
+++ src/types.cr
@@ -1,149 +1,90 @@
-macro active_record_define_not_null_for(type, name)
-  {{type.id}} {{name.id}}
-    def not_null! : {{name}}
-      self as {{name}}
-    end
-
-    def null?
-      false
-    end
-
-    {{type.id}} Null
-      def not_null! : NoReturn
-        ::raise ActiveRecord::NullCheckFailed.new("It is {{name.id}}::Null")
-      end
-
-      def inspect
-        "Null(#{{{type.id.stringify}}})"
+module ActiveRecord
+  macro define_not_null_for(type, name)
+    {{type.id}} {{name.id}}
+      def not_null! : {{name}}
+        self as {{name}}
       end
 
       def null?
-        true
+        false
       end
+
+      {{type.id}} Null
+        def not_null! : NoReturn
+          ::raise ActiveRecord::NullCheckFailed.new("It is {{name.id}}::Null")
+        end
+
+        def inspect
+          "Null(#{{{type.id.stringify}}})"
+        end
+
+        def null?
+          true
+        end
+      {{:end.id}}
     {{:end.id}}
-  {{:end.id}}
-end
-
-struct Int
-  def self.null_class
-    Null
   end
 
-  struct Null
-    include Comparable(Int)
+  macro register_type(kind, ty, default=nil, comparable=false, to_s="")
+    {{kind.id}} {{ty.id}}
+      def self.null_class
+        Null
+      end
 
-    def to_s(io)
-      io << ""
-    end
+      def ==(other : Null)
+        self == {{default}}
+      end
 
-    macro method_missing(name, args, block)
-      0.{{name.id}}({{args.argify}}) {{block}}
-    end
-  end
+      {{kind.id}} Null
+        {% if comparable %}include Comparable({{ty.id}}){% end %}
 
-  include Comparable(Null)
+        def to_s(io)
+          io << {{to_s}}
+        end
 
-  def <=>(other : Null)
-    self <=> 0
-  end
-end
+        def ==(other : self)
+          true
+        end
 
-class String
-  def self.null_class
-    Null
-  end
+        def ==(other)
+          {{default}} == other
+        end
 
-  def ==(other : Null)
-    self == ""
-  end
+        macro method_missing(name, args, block)
+          {{default}}.\{{name.id}}(\{{args.argify}}) \{{block}}
+        end
+      {{:end.id}}
 
-  class Null
-    def to_s(io)
-      io << ""
-    end
+      {% if comparable %}
+        include Comparable({{ty.id}})
 
-    def ==(other : self)
-      true
-    end
-
-    def ==(other : String)
-      other == self
-    end
-
-    macro method_missing(name, args, block)
-      "".{{name.id}}({{args.argify}}) {{block}}
-    end
+        def <=>(other : Null)
+          self <=> {{default}}
+        end
+      {% end %}
+    {{:end.id}}
   end
 end
 
-struct Time
-  def self.null_class
-    Null
-  end
+ActiveRecord.register_type :struct, Int, default: 0, comparable: true
+ActiveRecord.define_not_null_for(:struct, Int)
+ActiveRecord.define_not_null_for(:struct, Int8)
+ActiveRecord.define_not_null_for(:struct, UInt8)
+ActiveRecord.define_not_null_for(:struct, Int16)
+ActiveRecord.define_not_null_for(:struct, UInt16)
+ActiveRecord.define_not_null_for(:struct, Int32)
+ActiveRecord.define_not_null_for(:struct, UInt32)
+ActiveRecord.define_not_null_for(:struct, Int64)
+ActiveRecord.define_not_null_for(:struct, UInt64)
 
-  struct Null
-    include Comparable(Time)
+ActiveRecord.register_type :class, String, default: ""
+ActiveRecord.define_not_null_for(:class, String)
 
-    def to_s(io)
-      io << ""
-    end
+ActiveRecord.register_type :struct, Time, default: Time.new(0), comparable: true
+ActiveRecord.define_not_null_for(:struct, Time)
 
-    macro method_missing(name, args, block)
-      Time.new(0).{{name.id}}({{args.argify}}) {{block}}
-    end
-  end
-
-  include Comparable(Null)
-
-  def <=>(other : Null)
-    self <=> Time.new(0)
-  end
-end
-
-struct Bool
-  def self.null_class
-    Null
-  end
-
-  def ==(other : Null)
-    false
-  end
-
-  struct Null
-    def to_s(io)
-      io << ""
-    end
-
-    def ==(other : self)
-      true
-    end
-
-    def ==(other)
-      false == other
-    end
-
-    macro method_missing(name, args, block)
-      false.{{name.id}}({{args.argify}}) {{block}}
-    end
-  end
-end
-
-active_record_define_not_null_for(:struct, Int)
-
-active_record_define_not_null_for(:struct, Int8)
-active_record_define_not_null_for(:struct, UInt8)
-active_record_define_not_null_for(:struct, Int16)
-active_record_define_not_null_for(:struct, UInt16)
-active_record_define_not_null_for(:struct, Int32)
-active_record_define_not_null_for(:struct, UInt32)
-active_record_define_not_null_for(:struct, Int64)
-active_record_define_not_null_for(:struct, UInt64)
-
-active_record_define_not_null_for(:class, String)
-
-active_record_define_not_null_for(:struct, Time)
-
-active_record_define_not_null_for(:struct, Bool)
+ActiveRecord.register_type :struct, Bool, default: false
+ActiveRecord.define_not_null_for(:struct, Bool)
 
 module ActiveRecord
   alias IntTypes = Int8 | UInt8 | Int16 | UInt16 | Int32 | UInt32 | Int64 | UInt64 | Int::Null
