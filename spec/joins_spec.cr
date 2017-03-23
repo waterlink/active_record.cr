ca847bf1f41db3bae8eae6a7c8d9413dedbc16fa
Oleksii Fedorov <waterlink000@gmail.com>
2017-03-23 23:17:50 +0100

    Finish implementing one-to-one

--- spec/joins_spec.cr
+++ spec/joins_spec.cr
@@ -13,6 +13,19 @@ ActiveRecord::NullAdapter.register_join_query(
   PersonWithPostsJoinQuery.new
 )
 
+class PersonWithExampleJoinQuery < ActiveRecord::NullAdapter::JoinQuery
+  def call(base_record, foreign_record)
+    return  base_record &&
+            foreign_record &&
+            base_record["id"] == foreign_record["person_id"]
+  end
+end
+
+ActiveRecord::NullAdapter.register_join_query(
+  "people.id = example_models.person_id",
+  PersonWithExampleJoinQuery.new
+)
+
 class PersonWithPosts < ActiveRecord::Join
   one Person, id
   many Post, author_id
@@ -110,66 +123,65 @@ describe "joins - one to one" do
     end
   end
 
-  pending "allows to get one person with one post" do
+  it "allows to get one person with one foreign record" do
     # ARRANGE
     person = new_person.create
-    post = Post.create({
-      "title" => "hello",
-      "content" => "world",
-      "author_id" => person.id
+    example = ExampleModel.create({
+      "name" => "hello",
+      "person_id" => person.id
     })
 
     # ACT
-    actual = PersonWithPosts.get(person.id)
+    actual = PersonWithExample.get(person.id)
 
     # ASSERT
     actual.person.should eq(person)
-    actual.posts.should eq([post])
+    actual.example_model.should eq(example)
   end
 
-  pending "allows to get one person with two posts" do
+  it "allows to get one person with one foreign record when there are multiple" do
     # ARRANGE
     person = new_person.create
-    post_a = Post.create({
-      "title" => "post a",
-      "content" => "hi",
-      "author_id" => person.id
+    example_a = ExampleModel.create({
+      "name" => "example a",
+      "person_id" => person.id
     })
-    post_b = Post.create({
-      "title" => "post b",
-      "content" => "hi",
-      "author_id" => person.id
+    example_b = ExampleModel.create({
+      "name" => "example b",
+      "person_id" => person.id
     })
 
     # ACT
-    actual = PersonWithPosts.get(person.id)
+    actual = PersonWithExample.get(person.id)
 
     # ASSERT
     actual.person.should eq(person)
-    actual.posts.should eq([post_a, post_b])
+    actual.example_model.should eq(example_a)
   end
 
-  pending "allows to get one person with one post and without posts of other" do
+  it "allows to get one person with one example and without example of other" do
     # ARRANGE
     person = new_person.create
-    post_a = Post.create({
-      "title" => "post a",
-      "content" => "hi",
-      "author_id" => person.id
+    example_a = ExampleModel.create({
+      "name" => "example a",
+      "person_id" => person.id
     })
 
     other = new_person.create
-    post_b = Post.create({
-      "title" => "post b",
-      "content" => "hi",
-      "author_id" => other.id
+    example_b = ExampleModel.create({
+      "name" => "example b",
+      "person_id" => other.id
     })
 
     # ACT
-    actual = PersonWithPosts.get(person.id)
+    actual = PersonWithExample.get(person.id)
+    other_actual = PersonWithExample.get(other.id)
 
     # ASSERT
     actual.person.should eq(person)
-    actual.posts.should eq([post_a])
+    actual.example_model.should eq(example_a)
+
+    other_actual.person.should eq(other)
+    other_actual.example_model.should eq(example_b)
   end
 end
